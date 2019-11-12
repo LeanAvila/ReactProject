f778db73ff4e6537dae3a444be07318527aa0966
Leandro Avila <leandro.avila.212@gmail.com>
2019-11-12 15:07:59 -0300

    try again

--- myapp/client/src/index.js
+++ myapp/client/src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import PageLogin from './pages/pageLogin'
+import SignUp from './pages/signup'
+import HomePage from './pages/homePage';
+import Llamada from './Components/fetchCity'
+import store from './redux/store'
+const Routes =
+        <div
+       >
+           <Router>
+               <Switch>
+                       <Route exact path="/" component={HomePage}/>
+                       <Route exact path="/login" component={PageLogin}/>
+                       <Route exact path="/signup" component={SignUp}/>
+                       <Route exact path="/llamada" component={Llamada}/>
+               </Switch>
+           </Router>
+       </div>
 
-import Routes from './pages/routes';
-
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(Routes, document.getElementById('root'));
